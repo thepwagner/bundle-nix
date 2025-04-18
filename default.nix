c1388d4be180a51f37b4bb7225471dbf575f9fec
Peter Wagner <1559510+thepwagner@users.noreply.github.com>
2025-04-18 19:46:06 -0400

    more

--- default.nix
+++ default.nix
@@ -1,38 +1,64 @@
 let
-  pkgs =
-    import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/2631b0b7abcea6e640ce31cd78ea58910d31e650.tar.gz") {
-    };
+  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/2631b0b7abcea6e640ce31cd78ea58910d31e650.tar.gz") {};
   fs = pkgs.lib.fileset;
+  bundlenix = pkgs.stdenv.mkDerivation {
+    name = "bundlenix-build";
+    src = fs.toSource {
+      root = ./.;
+      fileset = fs.unions [
+        ./.cargo
+        ./src
+        ./vendor
+        ./Cargo.toml
+        ./Cargo.lock
+      ];
+    };
+    buildInputs = [pkgs.cargo];
+    buildPhase = ''
+      cargo build
+    '';
+    installPhase = ''
+      mkdir -p $out/bin
+      find target
+      cp target/debug/bundle-nix $out/bin/bundle-nix
+    '';
+  };
+  gems = pkgs.stdenvNoCC.mkDerivation {
+    name = "gems.drv";
+    outputHashMode = "text";
+    outputHashAlgo = "sha256";
+    requiredSystemFeatures = ["recursive-nix"];
+
+    src = fs.toSource {
+      root = ./.;
+      fileset = fs.unions [
+        ./Gemfile
+        ./Gemfile.lock
+      ];
+    };
+    buildInputs = [pkgs.nix bundlenix];
+
+    buildPhase = ''   
+      ${bundlenix}/bin/bundle-nix Gemfile.lock > derivation.nix
+    '';
+    installPhase = ''
+      cp $(nix-instantiate derivation.nix --arg pkgs 'import ${pkgs.path} {}') $out
+    '';
+  };
+
+  ruby = pkgs.stdenvNoCC.mkDerivation {
+    name = "ruby";
+    buildInputs = [pkgs.ruby];
+    src = fs.toSource {
+      root = ./.;
+      fileset = fs.unions [
+        ./run.rb
+      ];
+    };
+    installPhase = ''
+      mkdir $out
+      cp -r $src $out
+    '';
+  };
 in
-  with pkgs;
-    builtins.outputOf
-    (stdenv.mkDerivation {
-      name = "gems.drv";
-      outputHashMode = "text";
-      outputHashAlgo = "sha256";
-      requiredSystemFeatures = ["recursive-nix"];
-
-      src = fs.toSource {
-        root = ./.;
-        fileset = fs.unions [
-          ./.cargo
-          ./src
-          ./vendor
-          ./Cargo.toml
-          ./Cargo.lock
-
-          ./Gemfile
-          ./Gemfile.lock
-        ];
-      };
-
-      buildInputs = [nix cargo];
-
-      buildPhase = ''   
-        cargo run Gemfile.lock > derivation.nix
-      '';
-
-      installPhase = ''
-        cp $(nix-instantiate derivation.nix --arg pkgs 'import ${pkgs.path} {}') $out
-      '';
-    }).outPath "out"
+  builtins.outputOf(ruby).outPath "out"
---
+++ run.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+
+require "bigdecimal"
+require "meow-bundler"
