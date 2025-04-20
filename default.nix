0c642350af401fffcdcbb86fdcfa419954529531
Peter Wagner <1559510+thepwagner@users.noreply.github.com>
2025-04-20 08:36:40 -0400

    itch scratched

--- default.nix
+++ default.nix
@@ -1,6 +1,7 @@
 let
   pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/2631b0b7abcea6e640ce31cd78ea58910d31e650.tar.gz") {};
   fs = pkgs.lib.fileset;
+
   bundlenix = pkgs.stdenv.mkDerivation {
     name = "bundlenix-build";
     src = fs.toSource {
@@ -23,8 +24,10 @@ let
       cp target/debug/bundle-nix $out/bin/bundle-nix
     '';
   };
-  gems = pkgs.stdenvNoCC.mkDerivation {
+
+  gemsDrv = pkgs.stdenvNoCC.mkDerivation {
     name = "gems.drv";
+    __contentAddressed = true;
     outputHashMode = "text";
     outputHashAlgo = "sha256";
     requiredSystemFeatures = ["recursive-nix"];
@@ -38,7 +41,7 @@ let
     };
     buildInputs = [pkgs.nix bundlenix];
 
-    buildPhase = ''   
+    buildPhase = ''
       ${bundlenix}/bin/bundle-nix Gemfile.lock > derivation.nix
     '';
     installPhase = ''
@@ -46,19 +49,54 @@ let
     '';
   };
 
-  ruby = pkgs.stdenvNoCC.mkDerivation {
+  rubyVendor = builtins.outputOf(gemsDrv).outPath "out";
+
+  rubyApp = pkgs.stdenvNoCC.mkDerivation {
     name = "ruby";
-    buildInputs = [pkgs.ruby];
     src = fs.toSource {
       root = ./.;
       fileset = fs.unions [
         ./run.rb
       ];
     };
-    installPhase = ''
+    buildPhase = ''
       mkdir $out
-      cp -r $src $out
+      ln -s $src/* $out
+    '';
+    # TODO: this does not work and I'm going to give up on why not
+    installPhase = ''
+      mkdir -p $out/vendor/cache
+      ln -s ${builtins.outputOf(gemsDrv).outPath "out"} $out/vendor/cache
     '';
   };
+
+  cratesDrv = pkgs.stdenvNoCC.mkDerivation {
+    name = "crates.drv";
+    __contentAddressed = true;
+    outputHashMode = "text";
+    outputHashAlgo = "sha256";
+    requiredSystemFeatures = ["recursive-nix"];
+
+    src = fs.toSource {
+      root = ./.;
+      fileset = fs.unions [
+        ./Cargo.toml
+        ./Cargo.lock
+      ];
+    };
+    buildInputs = [pkgs.nix bundlenix];
+
+    buildPhase = ''
+      ${bundlenix}/bin/bundle-nix Cargo.lock > derivation.nix
+    '';
+    installPhase = ''
+      cp $(nix-instantiate derivation.nix --arg pkgs 'import ${pkgs.path} {}') $out
+    '';
+  };
+  cargoVendor = builtins.outputOf(cratesDrv).outPath "out";
 in
-  builtins.outputOf(ruby).outPath "out"
+  {
+    inherit bundlenix;
+    inherit gemsDrv rubyVendor rubyApp;
+    inherit cratesDrv cargoVendor;
+  }
